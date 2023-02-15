fc5d12735396a529552e4cb050fa2c4fb8e702f1
20Emi <iop.emilysanchez1@gmail.com>
2023-02-15 09:17:19 -0800

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <studio.h>
+#include <stdio.h>
 /**
  *main - Entry point
- *peturn: Always 0 (Success)
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
