79720d6312416239a0ff0b7cd70a96ff17151e15
20Emi <iop.emilysanchez1@gmail.com>
2023-02-15 10:21:38 -0800

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,11 +5,11 @@
  */
 int main(void)
 {
-printf("size of char: %zu bytes\n", sizeof(char));
-printf("Size of int: %zu bytes\n", sizeof(int));
-printf("Size of long int: %zu bytes\n", sizeof(long));
-printf("Size of long long int: %zu bytes\n", sizeof(long));
-printf("Size of float: %zu bytes\n", sizeof(float));
+printf("size of char: %zu byte(s)\n", sizeof(char));
+printf("Size of int: %zu byte(s)\n", sizeof(int));
+printf("Size of long int: %zu byte(s)\n", sizeof(long));
+printf("Size of long long int: %zu byte(s)\n", sizeof(long));
+printf("Size of float: %zu byte(s)\n", sizeof(float));
 
 return (0);
 }
