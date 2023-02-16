737e4a7a833752fb41fa1cc5996069ab1e2222d9
20Emi <iop.emilysanchez1@gmail.com>
2023-02-16 13:20:31 -0800

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - start
+ * Return: always 0
+ *
+ *
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		putchar(x);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
