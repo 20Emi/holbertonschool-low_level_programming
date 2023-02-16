97add133b4c1ec4a57a8fa7580c7e9ab2f94e21f
20Emi <iop.emilysanchez1@gmail.com>
2023-02-16 12:43:00 -0800

    8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - start
+ * Return: always 0
+ *
+ *
+ */
+int main(void)
+{
+	char x;
+	char y;
+
+	for (x = 48; x <= 57; x++)
+	for (y = 97; y <= 102; y++)
+	{
+	putchar(x);
+}
+
+	putchar('\n');
+
+	return (0);
+}
