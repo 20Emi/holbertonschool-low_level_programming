5da85a7fbff58de96b515e3ac4226f661fa6adc4
20Emi <iop.emilysanchez1@gmail.com>
2023-02-16 15:03:32 -0800

    9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -12,14 +12,15 @@ int main(void)
 	for (x = 48; x <= 57; x++)
 	{
 		putchar(x);
-		if (x == 9)
-		{
+		if (x == 57)
+	{
 		break;
-		}
-		putchar(',');
-		putchar(' ');
+	}
+	putchar(44);
+	putchar(32);
 	}
 	putchar('\n');
 
 	return (0);
+
 }
