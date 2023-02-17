fcd1f185ac840d6bb0ac43cc2eb5e4664f0581e9
20Emi <iop.emilysanchez1@gmail.com>
2023-02-17 10:13:37 -0800

    3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - start
+ * Return: always 0
+ *
+ *
+ */
+
+int
+_islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+		return (0);
+
+}
