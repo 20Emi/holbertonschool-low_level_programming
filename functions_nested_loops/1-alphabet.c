44dead2f153049bde60142c1b88593496b66ae97
20Emi <iop.emilysanchez1@gmail.com>
2023-02-17 06:30:05 -0800

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - start
+ * Return: always 0
+ *
+ *
+ */
+int
+main(void)
+{
+	char x;
+
+	for (x = 'a'; x >= 'z'; x++)
+	{
+		_putchar (x);
+	}
+		_putchar ('\n');
+
+		return (0);
+}
