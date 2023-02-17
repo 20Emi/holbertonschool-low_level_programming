101f40999ccc8b3fe3c14f6e4359754163887e91
20Emi <iop.emilysanchez1@gmail.com>
2023-02-17 08:24:23 -0800

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - start
+ * Return: always 0
+ *
+ *
+ */
+void
+print_alphabet_x10(void)
+
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		_putchar(x);
+		x++;
+	scanf("%c", &x);
+	}
+
+
+		_putchar('\n');
+
+}
