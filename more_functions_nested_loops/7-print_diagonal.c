90568ac33bb03b8a97279b5f59acf7c82755970a
20Emi <iop.emilysanchez1@gmail.com>
2023-02-23 04:17:47 -0800

    7-print_diagonal.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_diagonal - check the code
+ *
+ * @n: numero
+ * Return: Always 0.
+ */
+void
+print_diagonal(int n)
+{
+	int dig;
+	int ls;
+
+	if (n < 0)
+	{
+		_putchar('\n');
+	}
+	else if (n == 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (dig = 1; dig <= n; dig++)
+		{
+			for (ls = dig; ls >= n; ls++)
+			{
+				if (ls != 1)
+				{
+					_putchar (' ');
+				}
+			}
+
+		}
+	}
+}
