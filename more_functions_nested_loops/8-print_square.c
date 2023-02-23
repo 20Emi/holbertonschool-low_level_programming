32702c2f6395d38eddb31f6409153073e83756f9
20Emi <iop.emilysanchez1@gmail.com>
2023-02-23 05:46:10 -0800

    8-print_square.c

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -11,14 +11,17 @@ print_square(int size)
 {
 	int y, x;
 
+	if (size == 0)
+	{
+		_putchar('\n');
+	}
 	for (y = 0; y < size; y++)
 	{
 		for (x = 0; x < size; x++)
 		{
-			_putchar ('#');
+			_putchar('#');
 		}
 	_putchar('\n');
 	}
-
+	_putchar('\n');
 }
-
