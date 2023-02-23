9dc910be58397615ba8cedc658f361f0c9293585
20Emi <iop.emilysanchez1@gmail.com>
2023-02-23 05:20:28 -0800

    8-print_square.c

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * @size: numero
+ * Return: Always 0.
+ */
+void
+print_square(int size)
+{
+	int y, x;
+
+	for (y = 0; y < size; y++)
+	{
+		for (x = 0; x < size; x++)
+		{
+			_putchar ('#');
+		}
+	_putchar('\n');
+	}
+
+}
+
