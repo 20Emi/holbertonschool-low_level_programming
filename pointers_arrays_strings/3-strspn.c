7ef22bfa3a3ea9f77e0255049fe70890f95d5ff5
20Emi <iop.emilysanchez1@gmail.com>
2023-03-02 12:18:04 -0800

    3-strspn.c

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+unsigned int
+_strspn(char *s, char *accept)
+{
+	unsigned int one = 0;
+	unsigned int doo = 0;
+	unsigned int a = 0;
+
+	while (s[one])
+	{
+		while (accept[doo])
+		{
+			if (s[one] == accept[doo])
+			{
+				a++;
+				one++;
+				doo = -1;
+			}
+			doo++;
+		}
+		doo = 0;
+		if (a != 0)
+		{
+			break;
+		}
+	}
+	return (a);
+}
+
