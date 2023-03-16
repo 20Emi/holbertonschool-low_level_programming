b6342a1c631ef1770ea44c81fecc7feb92669fb9
20Emi <iop.emilysanchez1@gmail.com>
2023-03-15 18:55:41 -0700

    task 2

---
+++ 2-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (if is success)
+ **/
+int main(void)
+{
+	int n;
+
+	int e;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	e = n % 10;
+	if (e > 5)
+	{
+	printf("Last digit of %d is %d and is greater than 5\n", n, e);
+	}
+	else if (e == 0)
+	{
+	printf("Last digit of %d is %d and is 0\n", n, e);
+	}
+	else
+	{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, e);
+	}
+	return (0);
+}
---
+++ 2-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0 (if is success)
+ **/
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+	return (0);
+}
