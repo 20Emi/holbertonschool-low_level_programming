1d94687b80abbc31086ea8ec927cc48576c44ab4
20Emi <iop.emilysanchez1@gmail.com>
2023-03-16 09:50:52 -0700

    task 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -27,7 +27,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	}
 
-	ing = malloc(sizeof(char *) * (strlen(s1) + n + 1));
+	ing = (char *)malloc((strlen(s1) + n + 1));
 
 	if (ing == NULL)
 	{
