91d9d8c563260ef1c4bd1553218b88ba982d73dd
20Emi <iop.emilysanchez1@gmail.com>
2023-03-16 10:15:57 -0700

    2-calloc.c

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int a;
+	char *oc;
+
+	if (nmemb == 0)
+	{
+		return (NULL);
+	}
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	oc = malloc(size * nmemb);
+
+	if (oc == NULL)
+	{
+		return (NULL);
+	}
+
+	for (a = 0; oc[a] != '\0'; a++)
+	{
+		oc[a] = 0; /*The memory is set to zero*/
+	}
+
+	return (oc);
+}
+
