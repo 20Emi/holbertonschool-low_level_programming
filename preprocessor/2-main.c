d78b02e807151bb0864694727d8c1813e119fc04
20Emi <iop.emilysanchez1@gmail.com>
2023-03-17 08:08:35 -0700

    task 2

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 
 /**
- *main - prints the name of the file it was compiled from
+ * main -  prints the name of the file it was compiled from
  *
  * Return: 0
  */
 
-int main()
+int main(void)
 {
-	printf("%s\n"; 2-main.c);
+	printf("%s\n", __FILE__);
+
+	return (0);
 }
