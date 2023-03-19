96a7f3dc0cd564231f158c30db01f0bab4f4bbf7
20Emi <iop.emilysanchez1@gmail.com>
2023-03-19 13:43:46 -0700

    update README.md

---
+++ 5-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *@a: variable
+ *@b: variable
+ * Return: Always 0.
+ */
+
+int
+mul(int a, int b)
+{
+	int mul;
+	{
+		mul = a * b;
+			return (mul);
+	}
+}
