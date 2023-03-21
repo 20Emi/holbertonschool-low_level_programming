3b992c8801e8135f5eabfa42c1950bd013eb3a64
20Emi <iop.emilysanchez1@gmail.com>
2023-03-21 07:56:12 -0700

    update README.md

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,18 +8,16 @@
  *@owner: third element
  *
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 /*task 1*/
 void init_dog(struct dog *d, char *name, float age, char *owner);
 /*task 2*/
 void print_dog(struct dog *d);
-/*task 3*/
-typedef struct dog dog_t;
 /*task 4*/
 dog_t *new_dog(char *name, float age, char *owne);
 /*task 5*/
