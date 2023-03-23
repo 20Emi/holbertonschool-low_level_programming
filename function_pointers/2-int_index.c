9372c3385a2631a206fc29426954ddce68d7bcfe
20Emi <iop.emilysanchez1@gmail.com>
2023-03-22 18:08:39 -0700

    update README.md

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -12,10 +12,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int a = 0, inx;
 
-	if (size <= 0)
+	if (array == NULL || cmp == NULL)
 		return (-1);
 
-	if (array == NULL || cmp == NULL)
+	if (size <= 0)
 		return (-1);
 
 	while (a < size)
