90f1469a2f75370852694899186a0121f80718a7
20Emi <iop.emilysanchez1@gmail.com>
2023-03-23 09:30:13 -0700

    update RENAME.md

---
+++ function_pointers/3-calc.h
@@ -0,0 +1,27 @@
+#ifndef _calc_h_
+#define _calc_h_
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+    char *op;
+    int (*f)(int a, int b);
+} op_t;
+
+	/*sum*/
+int op_add(int a, int b);
+	/*difference*/
+int op_sub(int a, int b);
+	/*product*/
+int op_mul(int a, int b);
+	/*division*/
+int op_div(int a, int b);
+	/*remainder of the division*/
+int op_mod(int a, int b);
+
+#endif
