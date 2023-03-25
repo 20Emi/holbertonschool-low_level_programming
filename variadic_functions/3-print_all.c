e106fbc238c6a95db833b492fc4033a62aababb9
20Emi <iop.emilysanchez1@gmail.com>
2023-03-25 10:50:33 -0700

    update README.md

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,63 @@
+#include "variadic_functions.h"
+#include<stdio.h>
+#include<stdarg.h>
+
+void print_all(const char * const format, ...)
+{
+
+	/*a = va_arg(list, char);
+	 * print_c(a);*/
+
+	int i = 0, a, c;
+	double b; 
+	char *d;
+
+	va_list list;
+	va_start(list, format);
+
+	while (format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				a = va_arg(list, int);
+				print_c(a);
+				break;
+			case 'f':
+				b = va_arg(list, double);
+				print_f(b);
+				break;
+			case 'i':
+				c = va_arg(list, int);
+				 print_i(c);
+				 break;
+			case 's':
+				 d = va_arg(list, char *);
+				 print_s(d);
+				 break;
+		}
+		i++;
+	}
+	va_end(list);
+
+	printf("\n");
+}
+
+void print_c(char a)
+{
+	printf("%c, ", a);
+}
+void print_i(int a)
+{
+	printf("%d, ", a);
+}
+void print_f(float a)
+{
+	printf("%f, ", a);
+}
+void print_s(char *a)
+{
+	printf("%s", a);
+}
+
+
