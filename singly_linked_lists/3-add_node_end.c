72bffde88beb6fc0315042544e34695ebba9c1ee
20Emi <iop.emilysanchez1@gmail.com>
2023-04-05 18:23:20 -0700

    update README.md

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,26 +1,37 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
+ *add_node_end - function that adds a new node at the end
+ *@head: head
+ *@str: str
  *
- *
- *
+ *Return: lastnode
  */
 
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *lastnode = *head;
 	list_t *new = NULL;
 
 	new = (list_t *)malloc(sizeof(list_t));
 
-	if (*head == NULL)
+	if (new == NULL)
 	{
 		free(new);
-		return(NULL);
+		return (NULL);
 	}
 
 	new->str = strdup(str);
 	new->len = strlen(str);
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (*head);
+	}
 
 	while (lastnode->next != NULL)
 /*Recorre la lista hasta encontrar el último nodo*/
@@ -31,5 +42,5 @@ list_t *add_node(list_t **head, const char *str)
 	lastnode->next = new;
 /*Establece el puntero next del último nodo al nuevo nodo*/
 
-	return (*head);
+	return (lastnode->next);
 }
