6002fbb0640105335c198f746bcd92b04c52ef07
20Emi <iop.emilysanchez1@gmail.com>
2023-04-06 14:35:49 -0700

    update README.md

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+int
+sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	dlistint_t *nu = head;
+
+	while (nu != NULL)
+	{
+		sum += nu->n;
+/*agrega el valor del nodo a la suma*/
+		nu = nu->next;
+	}
+	nu = head->prev;
+/*recorre la lista hacia atras*/
+	while (nu != NULL)
+	{
+		sum += nu->n;
+		nu = nu->prev;
+/*retrocede al nodo anterior*/
+	}
+	return (sum);
+}
+
