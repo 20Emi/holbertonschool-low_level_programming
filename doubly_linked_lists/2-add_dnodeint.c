ff9ab7ab33d3af75c90327c6aea2b526361d5720
20Emi <iop.emilysanchez1@gmail.com>
2023-04-09 14:41:04 -0700

    update README.md

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,12 +1,7 @@
 #include "lists.h"
-/**
- *add_dnodeint - function that adds a new node at the beginning
- *@n: int
- *@head: points node
- *Return: *head
- *NULL: pointer to the previous node (when the new node is the head)
- */
-dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+
+dlistint_t
+*add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new = NULL;
 
@@ -20,12 +15,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->n = n;
 	new->prev = NULL;
 	new->next = *head;
-/**
- *'new->next = *head' - Establece el puntero al siguiente nodo como el puntero
- *a la cabeza de la lista actual.
- *'new->prev = NULL' - Establece el puntero al nodo anterior como NULL
- *porque este es el primer nodo en la lista.
- */
+
 	if (*head != NULL)
 	{
 		(*head)->prev = new;
