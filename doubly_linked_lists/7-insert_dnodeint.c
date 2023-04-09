7258bc7c99690ec08e67c1965d725e383514767f
20Emi <iop.emilysanchez1@gmail.com>
2023-04-09 15:49:52 -0700

    update README.md

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+dlistint_t
+*insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *newnode, *act = *h;
+	unsigned int a = 0;
+
+	newnode = (dlistint_t *)malloc(sizeof(dlistint_t));
+	newnode->n = n;
+	if (newnode == NULL)
+		return (NULL);
+
+	if (n == 0)
+	{
+		newnode->prev = NULL;
+		newnode->next = *h;
+		(*h)->prev = newnode;
+		*h = newnode;
+		return (newnode);
+	}
+	while (a < idx - 1 && act)
+	{
+		act = act->next;
+		a++;
+	}
+	if (act->next == NULL)
+	{
+		act->next = newnode;
+		newnode->prev = act;
+		newnode->next = NULL;
+		return (NULL);
+	}
+	newnode->prev = act;
+	newnode->next = act->next;
+	act->next = newnode;
+	newnode->next->prev = newnode;
+		return (newnode);
+}
