26369dd83defc09cbc51caeae89851fd455bce95
20Emi <iop.emilysanchez1@gmail.com>
2023-04-12 09:53:12 -0700

    update README.md

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fd1, fd2;
+	ssize_t out;
+	char *buff;
+
+	if (filename == NULL || letters == 0)
+		return (0);
+
+	fd1 = open(filename, O_RDWR);
+	if (fd1 == -1)
+		return (0);
+
+	buff = malloc(sizeof(char) * letters);
+
+	fd2 = read(fd1, buff, letters);
+
+	out = write(STDOUT_FILENO, buff, fd2);
+
+	close(fd1);
+
+	return (out);
+}
