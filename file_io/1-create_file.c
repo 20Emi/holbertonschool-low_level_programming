1d1801f8104eb5aa58c27d78f6658512e4424bc6
20Emi <iop.emilysanchez1@gmail.com>
2023-04-12 11:12:17 -0700

    update README.md

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -9,10 +9,10 @@
 int create_file(const char *filename, char *text_content)
 {
 	ssize_t fd1;
-	
+
 	if (filename == NULL)
 		return (-1);
-	
+
 	fd1 = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 	if (fd1 == -1)
 		return (-1);
