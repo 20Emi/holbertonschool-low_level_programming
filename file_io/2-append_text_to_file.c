6b0110f9591622bec69bfa649a5c16439c7ca23d
20Emi <iop.emilysanchez1@gmail.com>
2023-04-12 12:58:13 -0700

    update README.md

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	ssize_t fd1;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd1 = open(filename, O_CREAT | O_APPEND);
+	if (fd1 == -1)
+		return (-1);
+
+	if (text_content != NULL)
+		write(fd1, text_content, strlen(text_content));
+
+	close(fd1);
+
+	return (1);
+}
