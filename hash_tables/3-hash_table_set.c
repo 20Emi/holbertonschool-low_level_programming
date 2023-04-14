bab648f38173f91178aee9a2b278e824067b92a1
20Emi <iop.emilysanchez1@gmail.com>
2023-04-14 12:09:17 -0700

    update README.md

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+/**
+ *
+ *
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned int ind = key_index((const unsigned char *) key, ht->size);
+	hash_node_t *new;
+	hash_node_t	*act;
+
+	if (ht == NULL)
+		return (0);
+
+	if (ht->array[ind] == NULL) /*Verifica si la posición está vacía*/
+	{/*Cree un nuevo nodo*/
+		new = malloc(sizeof(hash_node_t));
+		new->key = strdup(key);
+		new->value = strdup(value);
+		new->next = NULL;
+
+		/*Agrega el nodo a la tabla hash*/
+		ht->array[ind] = new;
+	}
+	else
+	{
+		act = ht->array[ind];
+		while (act->next != NULL)
+			act = act->next;
+	}
+	act->next = new;
+	return (1);
+}
