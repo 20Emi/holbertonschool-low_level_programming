51c940bbfed95bbeb28e8a18552ba21a9e2262db
20Emi <iop.emilysanchez1@gmail.com>
2023-04-15 19:59:56 -0700

    update README.md

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -15,16 +15,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	act = strdup(value);
-
 	while (ht->array[a])
 	{
+		a++;
 		if (strcmp(ht->array[a]->key, key) == 0)
 		{
 			free(ht->array[a]->value);
 			ht->array[a]->value = act;
 			return (1);
 		}
-		a++;
 	}
 
 	new = malloc(sizeof(hash_node_t));
@@ -34,6 +33,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(act);
+		return (0);
+	}
 	new->value = act;
 	new->next = ht->array[ind];
 	ht->array[ind] = new;
