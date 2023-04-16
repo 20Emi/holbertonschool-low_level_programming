35fa5e6212266bef1f2406a6b8adf6a467cb3aeb
20Emi <iop.emilysanchez1@gmail.com>
2023-04-15 20:21:05 -0700

    update README.md

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -37,7 +37,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new->key = strdup(key);
 	if (!new->key)
 	{
-		free(act);
+		free(new);
 		return (0);
 	}
 	new->value = act;
